2b73c671659355a1b3d5b245f8c7f7701ccb5f25
Nicolás Passerini <npasserini@gmail.com>
2022-10-03 17:59:58 -0300

    Mover vista a la carpeta de views

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,34 +1,34 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import TablaExcel from '../components/TablaExcel.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import TablaExcel from "../views/TablaExcel.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "home",
+      component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
+      path: "/about",
+      name: "about",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: () => import("../views/AboutView.vue"),
     },
     {
-      path:'/axios-use',
-      name: 'axios-use',
-      component: () => import('../components/WeatherCard.vue')
-    }, 
+      path: "/axios-use",
+      name: "axios-use",
+      component: () => import("../components/WeatherCard.vue"),
+    },
     {
-      path:'/tabla',
-      name: 'tabla',
-      component: () => TablaExcel //import('../components/TablaExcel.vue')
-    }
-  ]
-})
+      path: "/tabla",
+      name: "tabla",
+      component: () => TablaExcel, //import('../components/TablaExcel.vue')
+    },
+  ],
+});
 
-export default router
+export default router;
---
+++ src/views/TablaExcel.vue
@@ -0,0 +1,43 @@
+<template>
+  <main>
+    <hot-table :settings="settings"></hot-table>
+  </main>
+</template>
+
+<script lang="ts">
+import { HotTable } from "@handsontable/vue3";
+import { registerAllModules } from "handsontable/registry";
+
+// register Handsontable's modules
+registerAllModules();
+
+export default {
+  data() {
+    return {
+      settings: {
+        data: [
+          // ['', 'Ford', 'Volvo', 'Toyota', 'Honda'],
+          ["2016", 10, 11, 12, 13],
+          ["2017", 20, 11, 14, 13],
+          ["2018", 30, 15, 12, 13],
+        ],
+        colWidths: [150, 150, 150, 150, 150, 150],
+        colHeaders: ["Ford", "Volvo", "Toyota", "Honda"],
+        rowHeaders: true,
+        licenseKey: "non-commercial-and-evaluation",
+        columns: [
+          { data: 0, type: "text" },
+          { data: 1, type: "text" },
+          { data: 2, type: "text" },
+          { data: 3, type: "text" },
+        ],
+      },
+    };
+  },
+  components: {
+    HotTable,
+  },
+};
+</script>
+
+<style src="handsontable/dist/handsontable.full.css"></style>
