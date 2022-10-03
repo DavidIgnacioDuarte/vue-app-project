743ae2c1ef9a836ce25300664c2dece0ca361ccd
Juan Lagostena <lagostenaj@aaip.gob.ar>
2022-10-03 14:14:55 -0300

    Modificacion para dejarlo andando de base

--- src/components/TablaExcel.vue
+++ src/components/TablaExcel.vue
@@ -13,26 +13,22 @@
   export default defineComponent({
     data() {
       return {
-        data: [
-          // ['', 'Ford', 'Volvo', 'Toyota', 'Honda'],
-          ['2016', 10, 11, 12, 13],
-          ['2017', 20, 11, 14, 13],
-          ['2018', 30, 15, 12, 13]
+          settings: {
+          data: [
+            // ['', 'Ford', 'Volvo', 'Toyota', 'Honda'],
+            ['2016', 10, 11, 12, 13],
+            ['2017', 20, 11, 14, 13],
+            ['2018', 30, 15, 12, 13]
           ],
           colWidths: [150, 150, 150, 150, 150, 150],
-          colHeaders: ['123', 'Ford', 'Volvo', 'Toyota', 'Honda'],
-          settings: {
-            data: "data",
-            height: 450,
-            rowHeaders: "true",
+            colHeaders: ['Ford', 'Volvo', 'Toyota', 'Honda'],
+            rowHeaders: true,
             licenseKey: 'non-commercial-and-evaluation',
             columns: [
+              { data: 0, type: "text" },
               { data: 1, type: "text" },
               { data: 2, type: "text" },
               { data: 3, type: "text" },
-              { data: 5, type: "text" },
-              { data: 6, type: "checkbox", className: "htCenter" },
-              { data: 7, type: "text" }
             ]
           }
       };
--- src/views/TablaView.vue
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-    import TablaExel from '../components/TablaExel.vue'
-</script>
-<template>
-    <TablaExel></TablaExel>
-</template>
- 
