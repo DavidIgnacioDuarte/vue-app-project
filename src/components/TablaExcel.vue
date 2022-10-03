43a74bc6fec71188f024dd45b22ca92dcd58ebf4
arielPerez447 <arielezequielperez@gmail.com>
2022-10-03 12:19:23 -0300

    uso de licencia free

--- src/components/TablaExcel.vue
+++ src/components/TablaExcel.vue
@@ -1,40 +1,36 @@
 <template>
-  <hot-table :settings="settings"></hot-table>
+  <hot-table
+  :rowHeaders= "true"
+  :colWidths="[100,100, 100, 100, 100]"
+  :colHeaders ="['Ford', 'Volvo', 'Toyota', 'Honda']"
+  licenseKey="non-commercial-and-evaluation">
+</hot-table>
+
+  <body>
+  </body>
+
 </template>
 
 <script lang="ts">
-  import { defineComponent } from 'vue';
   import { HotTable } from '@handsontable/vue3';
   import { registerAllModules } from 'handsontable/registry';
 
   // register Handsontable's modules
   registerAllModules();
 
-  export default defineComponent({
+  export default({
     data() {
       return {
-        data: [
-          // ['', 'Ford', 'Volvo', 'Toyota', 'Honda'],
-          ['2016', 10, 11, 12, 13],
-          ['2017', 20, 11, 14, 13],
-          ['2018', 30, 15, 12, 13]
-          ],
-          colWidths: [150, 150, 150, 150, 150, 150],
-          colHeaders: ['123', 'Ford', 'Volvo', 'Toyota', 'Honda'],
-          settings: {
-            data: "data",
-            height: 450,
-            rowHeaders: "true",
-            licenseKey: 'non-commercial-and-evaluation',
-            columns: [
-              { data: 1, type: "text" },
-              { data: 2, type: "text" },
-              { data: 3, type: "text" },
-              { data: 5, type: "text" },
-              { data: 6, type: "checkbox", className: "htCenter" },
-              { data: 7, type: "text" }
-            ]
-          }
+        // settings: {
+        //   rowHeaders: "true",
+        //   columns: [
+        //     { data: 1, type: "text" },
+        //     { data: 2, type: "text" },
+        //     { data: 3, type: "text" },
+        //     { data: 4, type: "text" },
+        //     { data: 5, type: "text" }
+        //     ]
+        //   }
       };
     },
     components: {
