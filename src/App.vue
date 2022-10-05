f69d2e7741b8937e96410c1da6cbee84e7acd4a9
arielPerez447 <arielezequielperez@gmail.com>
2022-10-05 15:07:58 -0300

    la tabla tiene dropdown

--- src/App.vue
+++ src/App.vue
@@ -3,38 +3,43 @@
 import { RouterLink, RouterView } from 'vue-router'
 
 </script>
-
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <RouterLink class="nav-link" to="/">Home</RouterLink>
-        </li>
-        <li class="nav-item">
-          <RouterLink class="nav-link" to="/about">About</RouterLink>
-        </li>
-        <li class="nav-item">
-          <RouterLink class="nav-link" to="/axios-use">Weather Card</RouterLink>
-        </li>
-        <li class="nav-item">
-          <RouterLink class="nav-link" to="/tabla">Ver Tabla</RouterLink>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-<header>
-</header>
-<body>
-  <div>
-    <RouterView></RouterView>
+  <header></header>
 
-  </div>
-</body>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <RouterLink class="nav-link" to="/">Home</RouterLink>
+          </li>
+          <li class="nav-item">
+            <RouterLink class="nav-link" to="/about">About</RouterLink>
+          </li>
+          <li class="nav-item">
+            <RouterLink class="nav-link" to="/axios-use">Weather Card</RouterLink>
+          </li>
+          <li class="nav-item">
+            <RouterLink class="nav-link" to="/tabla">Ver Tabla</RouterLink>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <body>
+    <div id="contenido">
+      <RouterView></RouterView>
+    </div>
+  </body>
 </template>
+<style>
+.contenido {
+
+  margin: 0px !important;
+}
+</style>
