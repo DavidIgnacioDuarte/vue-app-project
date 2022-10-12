0cd700cbf24f8ffc74728de702d22975e8c612d7
Nicolás Passerini <npasserini@gmail.com>
2022-10-12 09:30:18 -0300

    Cambiar los datos a objetos.

--- src/components/TablaExcel.vue
+++ src/components/TablaExcel.vue
@@ -14,7 +14,7 @@ registerAllModules();
 function semaphoreRenderer(instance, td, row, col, prop, value, cellProperties) {
   Handsontable.renderers.TextRenderer.apply(this, arguments);
 
-    const data = instance.getData()[row][31];
+    const data = instance.getData()[row][31]; // Magic number :-(
     if (data > 15) {
         td.style.backgroundColor = "red";
     } else if (data > 5) {
@@ -28,21 +28,21 @@ export default {
         return {
             settings: {
                 colWidths: [80, 90, 130, 250, 220, 200, 200, 200, 200, 200, 150, 150, 150, 200, 150, 150, 150, 150, 150, 150, 150, 350, 250, 150, 150, 250, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150],
-                colHeaders: ["ORDEN", "ESTADO", "PROXIMO_PASO", "N_DE_EXPEDIENTE", "SUJETO_OBLIGADO", "MINISTERIO_ORBITA", "FECHA_DE_PRESENTACION", "RECLAMANTE", "FORMA_DE_INGRESO", "PROVINCIA", "MOTIVO_DEL_RECLAMO", "SINTESIS_DEL_RECLAMO", "N_EXPEDIENTE_SOLICITUD", "N_NOTA_DE_TRASLADO", "1ER_VENCIMIENTO_NOTA_DESCARGO", "PASE_A_DNPDP", "VENCIMIENTO_RECLAMO", "N_DE ACTO ADMINISTRATIVO", "RESOLUCIÓN_DEL_RECLAMO", "FECHA_DE_ACTO_ADMINISTRATIVO", "SINTESIS_RESOLUCION", "OBSERVACIONES", "NOTIFICACION", "RESPONSABLE", "VENCIMIENTO_RESOLUCION", "CUMPLIMIENTO_RESOLUCION_NOTA", "INFORME_DE_CIERRE", "RESULTADO_INTIMACION", "Aux - Días Calculados", "Día,Aux-Fecha-Present", "Id-Provincia", "Aux-DateEng"],
+                colHeaders: ["ORDEN", "ESTADO", "PROXIMO_PASO", "N_DE_EXPEDIENTE", "SUJETO_OBLIGADO", "MINISTERIO_ORBITA", "FECHA_DE_PRESENTACION", "RECLAMANTE", "FORMA_DE_INGRESO", "PROVINCIA", "MOTIVO_DEL_RECLAMO", "SINTESIS_DEL_RECLAMO", "N_EXPEDIENTE_SOLICITUD", "N_NOTA_DE_TRASLADO", "1ER_VENCIMIENTO_NOTA_DESCARGO", "PASE_A_DNPDP", "VENCIMIENTO_RECLAMO", "N_DE ACTO ADMINISTRATIVO", "RESOLUCIÓN_DEL_RECLAMO", "FECHA_DE_ACTO_ADMINISTRATIVO", "SINTESIS_RESOLUCION", "OBSERVACIONES", "NOTIFICACION", "RESPONSABLE", "VENCIMIENTO_RESOLUCION", "CUMPLIMIENTO_RESOLUCION_NOTA", "INFORME_DE_CIERRE", "RESULTADO_INTIMACION", "Aux - Días Calculados", "Día","Aux-Fecha-Present", "Id-Provincia", "Aux-DateEng"],
                 width: "100%",
                 autoRowSize: true,
                 licenseKey: "non-commercial-and-evaluation",
                 columns: [
-                    { data: 0, type: "numeric", readOnly: true }, //Index
+                    { data: "ORDEN", type: "numeric", readOnly: true }, //Index
                     {
-                        data: 1, type: 'dropdown', source: ["Cerrado", "Abierto", "En Progreso"] //Estado
+                        data: "ESTADO", type: 'dropdown', source: ["Cerrado", "Abierto", "En Progreso"] //Estado
                     },
-                    { data: 2, type: "text" },
-                    { data: 3, type: "text" },
-                    { data: 4, type: "text" },
-                    { data: 5, type: "text" },
+                    { data: "PROXIMO_PASO", type: "text" },
+                    { data: "N_DE_EXPEDIENTE", type: "text" },
+                    { data: "SUJETO_OBLIGADO", type: "text" },
+                    { data: "MINISTERIO_ORBITA", type: "text" },
                     {
-                        data: 6,
+                        data: "FECHA_DE_PRESENTACION",
                         type: 'date',
                         dateFormat: 'DD/MM/YY',
                         correctFormat: true,
@@ -59,31 +59,32 @@ export default {
                             }
                         }
                     },
-                    { data: 7, type: "text" },
-                    { data: 8, type: "text" },
-                    { data: 9, type: "text" },
-                    { data: 10, type: "text" },
-                    { data: 11, type: "text" },
-                    { data: 12, type: "text" },
-                    { data: 13, type: "text" },
-                    { data: 14, type: "text" },
-                    { data: 15, type: "text" },
-                    { data: 16, type: "text" },
-                    { data: 17, type: "text" },
-                    { data: 18, type: "text" },
-                    { data: 19, type: "text" },
-                    { data: 20, type: "text" },
-                    { data: 21, type: "text" },
-                    { data: 22, type: "text" },
-                    { data: 23, type: "text" },
-                    { data: 24, type: "text" },
-                    { data: 25, type: "text" },
-                    { data: 26, type: "text" },
-                    { data: 27, type: "text" },
-                    { data: 28, type: "text" },
-                    { data: 29, type: "text" },
-                    { data: 30, type: "text" },
-                    { data: 31, type: "text" },
+                    { data: "RECLAMANTE", type: "text" },
+                    { data: "FORMA_DE_INGRESO", type: "text" },
+                    { data: "PROVINCIA", type: "text" },
+                    { data: "MOTIVO_DEL_RECLAMO", type: "text" },
+                    { data: "SINTESIS_DEL_RECLAMO", type: "text" },
+                    { data: "N_EXPEDIENTE_SOLICITUD", type: "text" },
+                    { data: "N_NOTA_DE_TRASLADO", type: "text" },
+                    { data: "1ER_VENCIMIENTO_NOTA_DESCARGO", type: "text" },
+                    { data: "PASE_A_DNPDP", type: "text" },
+                    { data: "VENCIMIENTO_RECLAMO", type: "text" },
+                    { data: "N_DE ACTO ADMINISTRATIVO", type: "text" },
+                    { data: "RESOLUCIÓN_DEL_RECLAMO", type: "text" },
+                    { data: "FECHA_DE_ACTO_ADMINISTRATIVO", type: "text" },
+                    { data: "SINTESIS_RESOLUCION", type: "text" },
+                    { data: "OBSERVACIONES", type: "text" },
+                    { data: "NOTIFICACION", type: "text" },
+                    { data: "RESPONSABLE", type: "text" },
+                    { data: "VENCIMIENTO_RESOLUCION", type: "text" },
+                    { data: "CUMPLIMIENTO_RESOLUCION_NOTA", type: "text" },
+                    { data: "INFORME_DE_CIERRE", type: "text" },
+                    { data: "RESULTADO_INTIMACION", type: "text" },
+                    { data: "Aux - Días Calculados", type: "text" },
+                    { data: "Día", type: "text" },
+                    { data: "Aux-Fecha-Present", type: "text" },
+                    { data: "Id-Provincia", type: "text" },
+                    { data: "Aux-DateEng", type: "text" },
                 ],
                 customBorders: true,
                 dropdownMenu: true,
@@ -103,8 +104,9 @@ export default {
 
     beforeMount() {
         Papa.parse("ejemplo_reclamos.csv", {
-        download: true,
-            complete: ({ data: [_, ... data]}) => {
+            download: true,
+            header: true,
+            complete: ({ data }) => {
                 this.swapHotData(data);
             },
         });
