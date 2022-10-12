69c609fdb7dc9ec4c30dab4771465266df63b702
Nicolás Passerini <npasserini@gmail.com>
2022-10-11 23:09:05 -0300

    Levantar listado asincrónicamente.

--- src/components/TablaExcel.vue
+++ src/components/TablaExcel.vue
@@ -1,10 +1,11 @@
 <template>
-    <hot-table :settings="settings"></hot-table>
+  <hot-table ref="hotTableComponent" :settings="settings"></hot-table>
 </template>
   
 <script lang="ts">
 import { HotTable, HotColumn } from "@handsontable/vue3";
 import { registerAllModules } from "handsontable/registry";
+import * as Papa from "papaparse";
 
 // register Handsontable's modules
 registerAllModules();
@@ -13,14 +14,6 @@ export default {
     data() {
         return {
             settings: {
-                data: [
-                    [1, "Cerrado", "Finalizado", "EX-2017-29999784-APN-AAIP", "Administración de Parques Nacionales", "Ministerio de Ambiente y Desarrollo Sostenible", "43066", "Emiliano Donadio", "E-MAIL", "Neuquén", "Silencio", "", "EX-2017-29999784-APN-AAIP", "PV-2017-30415871-APN-AAIP", "2017-07-12", "", 43374, "IF-2017-1809923-APN-AAIP", "Archivo", 43110, "Archivo por ampliacion de respuesta", "Se cerro declarando abstracto el reclamo por haberse contestado al requirente fuera de plazo al momento de solicitar el descargo", "GENE00028", "", "", "ASETTE", "", "", 43111, "Thursday", 43066, 14, 43374],
-                    [2, "Cerrado", "Finalizado", "EX-2017-30439758-APN-AAIP", "Registro Nacional de Puertos", "Ministerio de Transporte", "43068", "Arsenio Ruppel", "E-MAIL", "Santa Fe", "Silencio", "", "EX-2018-752574-APN-AAIP", "IF-2017-30633781-APN-AAIP", "2017 - 07 - 12", "", 43435, "IF-2018-01606094-APN-AAIP", "Archivo", 43109, "Archivo por ampliacion de respuesta", "Se cerro declarando abstracto el reclamo por haberse contestado al requirente fuera de plazo al momento de solicitar el descargo", "", "", "", "", "", "", 43115, "Monday", 43068, 20, 43435],
-                    [3, "Cerrado", "Finalizado", "EX-2017-30671310-APN-AAIP", "Administración de Parques Nacionales", "Ministerio de Ambiente y Desarrollo Sostenible", 43069, "Emiliano Donadio", "E-MAIL", "Neuquén", "Silencio", "", "EX-2017-30671310-APN-AAIP", "IF-2017-30678310-APN-AAIP", "2017 - 07 - 12", "", 43113, "IF-2017-33505235-APN-AAIP", "Archivo", 43087, "Archivo por ampliacion de respuesta", "Se cerro declarando abstracto el reclamo por haberse contestado al requirente fuera de plazo al momento de solicitar el descargo", "18/12 se notificó por mail al requirente", "", "", "", "", "", 43116, "Tuesday", 43069, 14, 43113],
-                    [4, "Cerrado", "Finalizado", "EX - 2017 - 30837261 - APN - AAIP", "Edesur S.A", "Empresa", 43070, "Miguel Angel Bolaños", "E - MAIL", "Buenos Aires", "Silencio", "", "SIN GDE", "NO 2017 - 31365223 - APN - AAIP", "14 / 12 / 2017", "", "43116", "IF - 2018 - 01605640 - APN - AAIP", "Archivo", 43109, "Archivo por ampliacion de respuesta", "Se cerro declarando abstracto el reclamo por haberse contestado al requirente fuera de plazo al momento de solicitar el descargo", "12 / 1 / 18 se notificó al reclamante y al sujeto obligado", "", "", "", "", "", 43117, "Wednesday", 43070, 1, 43116],
-                    [5, "Cerrado", "Finalizado", "EX - 2017 - 30827262 - APN - AAIP", "Gendarmería Nacional", "Ministerio de Seguridad", 43070, "Yrenarco Fernandez", "PAPEL", "Formosa", "Silencio", "", "SIN GDE", "NO - 2017 - 30879963 - APN - AAIP", "2017 - 08 - 12", "", 43116, "RESOL 2018 - 2 - APN - AAIP", "Intimacion", 43115, "Intimacion por denegatoria injustificada, Se hizo lugar el reclamo intimando a la entrega de la información.Por NO - 2018 - 03301230 - APN - SSAJ#MSG se informa que el 17 / 1 Gendarmería envió documentación al requirente", "Por NO - 2018 - 02771178 - APN - AAIP el 17 / 1 se notificó resolución a Seguridad", "", "", "Por Nota 2018 - 03301230APN - SSAJ#MSG contestan que gendarmería entrego información", "", "", "", 43117, "Wednesday", 43070, 8, 43116],
-                    [6, "Cerrado", "Finalizado", "EX - 2017 - 31370134 - APN - AAIP", "Edesur S.A", "Empresa", 43074, "Miguel Angel Bolaños", "E - MAIL", "Buenos Aires", "Silencio", "", "SIN GDE", "NO 2017 - 31373951 - APN - AAIP", "14 / 12 / 2017", "", 43118, "IF - 2018 - 01605943 - APN - AAIP", "Archivo", 43109, "Archivo por ampliacion de respuesta", "Se cerro declarando abstracto el reclamo por haberse contestado al requirente fuera de plazo al momento de solicitar el descargo", "12 / 1 / 18 se notificó al reclamante y al sujeto obligado", "", "", "", "", "", 43119, "Friday", 43074, 1, 43118]
-                ],
                 colWidths: [80, 90, 130, 250, 220, 200, 200, 200, 200, 200, 150, 150, 150, 200, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150],
                 colHeaders: ["ORDEN", "ESTADO", "PROXIMO_PASO", "N_DE_EXPEDIENTE", "SUJETO_OBLIGADO", "MINISTERIO_ORBITA", "FECHA_DE_PRESENTACION", "RECLAMANTE", "FORMA_DE_INGRESO", "PROVINCIA", "MOTIVO_DEL_RECLAMO", "SINTESIS_DEL_RECLAMO", "N_EXPEDIENTE_SOLICITUD", "N_NOTA_DE_TRASLADO", "1ER_VENCIMIENTO_NOTA_DESCARGO", "PASE_A_DNPDP", "VENCIMIENTO_RECLAMO", "N_DE ACTO ADMINISTRATIVO", "RESOLUCIÓN_DEL_RECLAMO", "FECHA_DE_ACTO_ADMINISTRATIVO", "SINTESIS_RESOLUCION", "OBSERVACIONES", "NOTIFICACION", "RESPONSABLE", "VENCIMIENTO_RESOLUCION", "CUMPLIMIENTO_RESOLUCION_NOTA", "INFORME_DE_CIERRE", "RESULTADO_INTIMACION", "Aux - Días Calculados", "Día,Aux-Fecha-Present", "Id-Provincia", "Aux-DateEng"],
                 width: 1000,
@@ -67,6 +60,21 @@ export default {
                 contextMenu: true,
             },
         };
+    },
+    methods: {
+        swapHotData: function (data) {
+        data.shift();
+        this.$refs.hotTableComponent.hotInstance.loadData(data);
+        },
+    },
+
+    beforeMount() {
+        Papa.parse("ejemplo_reclamos.csv", {
+        download: true,
+        complete: (results) => {
+            this.swapHotData(results.data);
+        },
+        });
     },
     components: {
         HotTable,
